85319d1c4e794cf9c2e6c7d16930013bd8ef113d
pitorreb <benjaminpitorre@atchik.com>
2022-04-26 17:20:01 +0200

    update logical (simplify)

--- src/app/custom/logical/logical.component.ts
+++ src/app/custom/logical/logical.component.ts
@@ -19,12 +19,15 @@ export class LogicalComponent implements OnInit {
     currentStorie: LogicalStorieModel;
     currentChoice = '';
 
+    nbChecked = 0;
+    nbUselessChecked = 0;
+
     ngOnInit(): void {
 
         this.logicalService.getData('assets/data/logical.json').subscribe(
             (response: HttpResponse<LogicalModel>) => {
                 this.logicalModel = response.body;
-                this.currentStorie = this.logicalModel.stories[0];
+                this.setRandomStorie();
             }
             , () => {
             }
@@ -34,18 +37,55 @@ export class LogicalComponent implements OnInit {
 
     verifyFact(index: number) {
         this.currentStorie.facts[index].verified = true;
+        this.nbChecked++;
+        if(this.currentStorie.facts[index].rule == 'NONE') {
+            this.nbUselessChecked++;
+        }
     }
 
     selectChoice(choice: string) {
         this.currentChoice = choice;
     }
 
-    changeStorie() {
-        this.currentChoice = '';
+    setRandomStorie() {
+        // make deep copy
+        let i  = 0;
+        if (Math.random() > 0.5) {
+            i = 1;
+        }
+        this.currentStorie = JSON.parse(JSON.stringify(this.logicalModel.stories[i]));
+        this.shuffle(this.currentStorie.facts);
     }
 
+    // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    }
+
+    raz() {
+        this.nbChecked = 0;
+        this.nbUselessChecked = 0;
+        this.currentChoice = '';
+        this.setRandomStorie();
+    }
+
+
     getFactClass(fact: LogicalFactModel) {
-        if (fact.verified && this.currentChoice != '') {
+        if (fact.verified && this.currentChoice !== '') {
             if (fact.rule === 'CONFIRM') {
                 return 'bg-success';
             }
